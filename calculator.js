cf6ebc086ec79f7ac0f082757ca73cc7d6cfe36c
AnshKumar <tech.anshkumar.28@gmail.com>
2026-09-05 23:45:56 +0530

    Initial assignment submission

--- calculator.js
+++ calculator.js
@@ -5,10 +5,18 @@ const num1 = Number(args[1]);
 const num2 = Number(args[2]);
 
 if (operation === "add") {
-  console.log("Result:", num1 + num2);
+    console.log("Result:", num1 + num2);
 } else if (operation === "subtract") {
-  console.log("Result:", num1 - num2);
+    console.log("Result:", num1 - num2);
+} else if (operation === "multiply") {
+    console.log("Result:", num1 * num2);
+} else if (operation === "divide") {
+    if (num2 === 0) {
+        console.log("Cannot divide by zero!");
+    } else {
+        console.log("Result:", num1 / num2);
+    }
 } else {
-  console.log("Invalid operation!");
-  console.log("Use: add or subtract");
+    console.log("Invalid operation!");
+    console.log("Use: add, subtract, multiply or divide");
 }
